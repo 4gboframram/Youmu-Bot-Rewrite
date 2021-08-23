3193453c4e0ced45781be803d3e03b28125ef12c
4gboframram <79847791+4gboframram@users.noreply.github.com>
2021-08-22 21:33:24 -0400

    Refactored all of the code

--- cogs/__init__.py
+++
@@ -1,1 +0,0 @@
-# filler shit so github doesn't forget to upload it again
--- cogs/embeds.py
+++
@@ -1,9 +0,0 @@
-from discord import Embed
-
-
-class YoumuEmbed(Embed):  # Creates a standard embed used by the bot
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.set_author(name="Youmu Bot",
-                        icon_url='https://cdn.discordapp.com/avatars/847655832169480222/16c78890f9383ec318b4560675410120.webp?size=2048',
-                        )
